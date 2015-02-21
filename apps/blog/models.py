cbae8fdde6d8185afa8a05bca33030e9b2f7c97f
Faris Chebib <faris@zaneprep.com>
2015-02-20 22:46:12 -0700

    added initial urls for api

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -3,8 +3,18 @@
 import datetime, arrow
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from saucesmere.models.mixins import TimestampMixin
 
 class Entry(TimestampMixin):
-    pass
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+    
+    published_on = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return u"{self.title} by {self.author.first_name}".format(self=self)
